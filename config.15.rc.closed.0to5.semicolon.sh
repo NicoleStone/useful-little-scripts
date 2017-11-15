3d96fbcf165c80ef445f4989e2a59514ba7247e1
Nicole Stone <NicoleStone@users.noreply.github.com>
2017-11-15 13:51:43 -0800

    Add files via upload

---
+++ config.15.rc.closed.0to5.semicolon.sh
@@ -0,0 +1,25 @@
+# If you want to specify the gene list as "genome", use "-1".
+# Use TAB to separate the three columns: coverage file<TAB>gene list<TAB>title
+# "title" will be shown in the figure's legend.
+
+#Fib
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fib_w1.038.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt         "Fib_w1.038"	150	black
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fib_w1.039.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt         "Fib_w1.039"	150	black
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fib_w1.040.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt         "Fib_w1.040"	150	black
+
+#Fibc
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fibc_d2.138.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "Fibc_d2.138"	150	red
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fibc_d2.143.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "Fibc_d2.143"	150	red
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.Fibc_d2.157.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "Fibc_d2.157"	150	red
+
+#GMT_w1
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMT_w1.063.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "GMT_w1.063"	150	green
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMT_w1.080.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "GMT_w1.080"	150	green
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMT_w1.084.mapped.bam.rem_dups.bam    /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt       "GMT_w1.084"	150	green
+
+#GMTc_w1
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMTc_w1.096.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt        "GMTc_w1.096"	150	green4
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMTc_w1.101.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt        "GMTc_w1.101"	150	green4
+/data/home/nstone/20170403_ATAC.align.final/aligned/bam.rem.dups/Browser.sort.Browser.sort.GMTc_w1.114.mapped.bam.rem_dups.bam     /data/home/nstone/20170605.ngsplot/3.rc.chem_closed.NGSplot.TSS.0to5.semicolon.txt        "GMTc_w1.114"	150	green4
+
+
