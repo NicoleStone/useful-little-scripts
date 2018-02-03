54e63eeca55690c216db698c4b1d054d5ef109d0
Nicole Stone <NicoleStone@users.noreply.github.com>
2018-02-02 17:13:12 -0800

    Add files via upload

---
+++ gff_from_3col_bed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#making gff for featureCounts input from ChIP-seq peak set, not for genome annotation
+for f in masterPeaks_*; do awk -F"\t" '{print $1 "\t" "method" "\t" "feature" "\t" $2 "\t" $3 "\t" "." "\t" "." "\t" "." "\t" "group" }' $f > $f.gff.txt; done
