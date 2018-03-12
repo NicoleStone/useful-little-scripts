8ad1af2d8108d844ab699bc36a2f3470d572014b
Nicole Stone <NicoleStone@users.noreply.github.com>
2018-03-12 13:09:18 -0700

    Update and rename 20180309_hopach.chip.R to run.hopach.20180309.R

--- run.hopach.20180309.R
+++ run.hopach.20180309.R
@@ -1,24 +1,17 @@
 #!/usr/bin/env Rscript
 require(hopach)
 
-#rigel
 setwd("/data/home/nstone/2018_hopach")
 
-#load chip:input data
-HMData <- read.table("chipover.input_log.CPM_Chip_Atac_SignificantPeaks_forR.txt", header=TRUE, sep="\t")
+#load normalized counts data, with first column identifyer of gene names, regions, etc.
+HMData <- read.table("log.CPM_SignificantPeaks.txt", header=TRUE, sep="\t")
 rownames(HMData)<-HMData[,1]
 HMData<-HMData[,20:28] #this depends on what columns you want to cluster based on
 hmat <- as.matrix(HMData) #make it a matrix
-#
 
 pept.dist <- distancematrix(hmat,"cor")
 pept.hobj <- hopach(hmat, dmat=pept.dist, clusters="best", initord="clust")
 
-NClust <- pept.hobj$clust$k
-Sizes <- pept.hobj$clust$sizes
-MedoidPeps <- pept.hobj$clust$medoids
-Order <- pept.hobj$clust$order
-
 makeoutput(hmat,pept.hobj,file="20180309_HopachResults.ChIP.txt")
 
-#### end rigel portion.
+####
