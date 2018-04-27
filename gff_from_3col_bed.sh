f11468675eed38fb5988b65b9084156fe4cccfa3
Nicole Stone <NicoleStone@users.noreply.github.com>
2018-04-26 20:22:45 -0700

    Update gff_from_3col_bed.sh

--- gff_from_3col_bed.sh
+++ gff_from_3col_bed.sh
@@ -1,4 +1,8 @@
+
+
 #!/bin/bash
 
 #making gff for featureCounts input from ChIP-seq peak set, not for genome annotation
-for f in masterPeaks_*; do awk -F"\t" '{print $1 "\t" "method" "\t" "feature" "\t" $2 "\t" $3 "\t" "." "\t" "." "\t" "." "\t" "group" }' $f > $f.gff.txt; done
+for f in masterPeaks_*; do awk -F"\t" '{print $1 "\t" "method" "\t" "exon" "\t" $2 "\t" $3 "\t" "." "\t" "." "\t" "." "\t" "group" }' $f > $f.gff.txt; done
+
+
