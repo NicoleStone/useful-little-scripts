759043f1d9675b6f79d3a4204ba7595c1b373652
Nicole Stone <NicoleStone@users.noreply.github.com>
2018-11-20 13:03:34 -0800

    Add files via upload

---
+++ make-a-symlink.sh
@@ -0,0 +1,5 @@
+#making a dropbox symlink
+ln -s ~/Dropbox\ \(Gladstone\) ~/DropboxGladstone
+# you do the first part on the terminal
+# then there will be a "DropboxGladstone" symlink henceforth
+# then you just change the path in your script from whatever/Dropbox (Stuff)/ to /whatever/DropboxGladstone/
