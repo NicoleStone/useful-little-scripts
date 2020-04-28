493102989fd2943eb35c698a51df7c7f9aa76072
Nicole Stone <NicoleStone@users.noreply.github.com>
2020-04-28 10:26:58 -0700

    Add files via upload

---
+++ debug_comments_to_stderr.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+GLOBAL_DEBUG = True # make this True if you want debugging comments printed to terminal, False if not
+def debugPrint(msg):
+	if GLOBAL_DEBUG:
+		print(msg)
+		
+debugPrint("hello")
